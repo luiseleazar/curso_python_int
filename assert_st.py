3d5556f698452d726502aaef8cdf34665a1aa23e
Luis Eleazar <eleanv05@gmail.com>
2021-09-15 18:46:41 -0700

    Trying assert statements

---
+++ assert_st.py
@@ -0,0 +1,18 @@
+def divisors(num):   
+    divisors = []
+    for i in range(1,num +1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+   
+    
+
+def run():
+    num = input("ingresa un numero: ")
+    assert num.isnumeric(), "Ingresa un número"
+    print(divisors(int(num))) 
+    print("terminó el programa")
+    
+
+if __name__ == '__main__':
+    run()
---
+++ debugging_assert.py
@@ -0,0 +1,21 @@
+def divisors(num):   
+    divisors = []
+    for i in range(1,num +1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+   
+    
+
+def run():
+
+    num = int(input("ingresa un numero: "))
+    print(divisors(num)) 
+    print("terminó el programa")
+
+
+
+    
+
+if __name__ == '__main__':
+    run()
